96004b663b0efd88426793cab0cf8bce0a5d9445
kamareee <kamar.uddin@hotmail.com>
2024-01-24 00:02:23 +1100

    Added initial project

---
+++ petclinic-app/src/main/java/com/kamareee/demopetclinic/PetclinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.kamareee.demopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicAppApplication.class, args);
+    }
+
+}

ed5f21d1d772cd0a1269660a3d4f2af264eba982
kamareee <kamar.uddin@hotmail.com>
2024-01-30 22:29:27 +1100

    Added Base entity as per original pet clinic design. Closes #10

--- pet-clinic-data/src/main/java/com/kamareee/demopetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/kamareee/demopetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.kamareee.demopetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/kamareee/demopetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/kamareee/demopetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.kamareee.demopetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
